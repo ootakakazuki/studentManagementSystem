4c95470ce097cd21687771adcfe51424055e077c
kuro <lllll1llllll1llll1@yahoo.co.jp>
2021-04-30 08:40:04 +0900

    配置換え

---
+++ src/studentdatbaseapp/Student.java
@@ -0,0 +1,75 @@
+package studentdatbaseapp;
+import java.util.Scanner;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int gradeYear;
+	private String studentID;
+	private String courses;
+	private int tuitionBalance = 0;
+	private static int costOfCource = 600;
+	private static int id = 1001;
+	
+	// コンストラクタ:生徒に名前と年を尋ねる
+	public Student() 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter student first name: ");
+		this.firstName = in.nextLine();
+
+		System.out.println("Enter student last name: ");
+		this.lastName = in.nextLine();
+		
+		System.out.println("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level:");
+		this.gradeYear = in.nextInt();
+		
+		
+		
+		setStudentID();
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+		
+	}
+	
+	// IDの生成
+	private void setStudentID() 
+	{
+		// id のインクリメント
+		id++;
+		// 学年 + ID で生成
+		this.studentID = gradeYear + "" + id;
+	}
+	
+	// 講義の登録
+	public void enroll() 
+	{
+		do {
+			System.out.println("Enter course to enroll (Q to quit): ");
+			Scanner in = new Scanner(System.in);
+			String course = in.nextLine();
+			if (!course.equals("Q"))
+			{
+				courses = courses + "\n" + course;
+				tuitionBalance = tuitionBalance + costOfCource;
+			}
+			else 
+			{
+				break;
+			}
+		} while (true);
+		System.out.println("ENROLLED IN: " + courses);
+		System.out.println("TUITION BALANCE: " + tuitionBalance);
+	}
+	
+	
+	
+
+	
+	
+	// 残高を見る
+	
+	// 授業料を払う
+	
+	// ステータス確認
+	
+}
---
+++ src/studentdatbaseapp/StudentDatabaseApp.java
@@ -0,0 +1,14 @@
+package studentdatbaseapp;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		Student stu1 = new Student();
+		// 新入生を何人加えたいか
+		stu1.enroll();
+		
+
+	}
+
+}
+
